7605b150f4d09434eda43aaa4a753725706c3a7e
Eder Coloma <edermaceno@gmail.com>
2020-02-24 20:18:07 -0300

    Picking numbers

---
+++ src/main/java/challenges/PickingNumbers.java
@@ -0,0 +1,51 @@
+package challenges;
+
+import static java.lang.Math.abs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given an array of integers, find and print the maximum number of integers 
+ * you can select from the array such that the absolute difference between any 
+ * two of the chosen integers is less than or equal to 1.
+ */
+public class PickingNumbers {
+
+	/**
+	 * Makes the constructor private so the class can not be instantiated.
+	 */
+	private PickingNumbers() {}
+	
+	/**
+	 * Check the possible lists of numbers for the highest number of elements between these lists.
+	 * 
+	 * @param a The list to be considered.
+	 * @return The highest number of elements between the possible lists.
+	 */
+    public static int pickingNumbers(final List<Integer> a) {
+    	
+    	int lasgestListSize = 0;
+    	final List<Integer> currentList = new ArrayList<>();
+    	
+    	a.sort(null);
+    	for(int i = 0; i < a.size(); i++) {
+    		 
+    		currentList.clear();
+    		currentList.add(a.get(i));
+    		
+    		for(int j = i + 1; j < a.size(); j++) {
+    			if(a.get(i) - a.get(j) == 0 || abs(a.get(i) - a.get(j)) == 1){
+    				currentList.add(a.get(j));
+        		}
+    		}
+    		
+    		if(currentList.size() > lasgestListSize) {
+    			lasgestListSize = currentList.size();
+    		}
+    	}
+    	
+    	return lasgestListSize;
+    }
+	        
+}
---
+++ src/test/java/challenges/PickingNumbersTest.java
@@ -0,0 +1,52 @@
+package challenges;
+
+import static  challenges.PickingNumbers.pickingNumbers;
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+/**
+ * Tests the methods of the {@link PickingNumbers} class.
+ */
+public class PickingNumbersTest {
+
+	/**
+	 * Tests the finding of the largest number of elements.
+	 */
+	@Test
+	public void pickingNumbersTest() {
+		final List<Integer> list = new ArrayList<>();
+		
+		list.add(1);
+		list.add(1);
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(5);
+		list.add(6);
+		list.add(7);
+		
+		assertEquals(4, pickingNumbers(list));
+	}
+	
+	/**
+	 * Tests the finding of the largest number of elements.
+	 */
+	@Test
+	public void pickingNumbersOtherTest() {
+		final List<Integer> list = new ArrayList<>();
+		
+		list.add(6);
+		list.add(4);
+		list.add(5);
+		list.add(3);
+		list.add(3);
+		list.add(1);
+		
+		assertEquals(3, pickingNumbers(list));
+	}
+	
+}
